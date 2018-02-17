852b3b6c30ee72c4b1ff4282cbf645ddd5d7e07e
Ivan Chapman <irchap99@gmail.com>
2018-02-17 07:53:00 -0800

    first commit friendFinder

---
+++ friendFinder/app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Michael',
+		photo: 'http://picture_Michael.jpg ',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'3',
+			'5',
+			'1',
+			'1',
+			'1',
+			'4'
+		]
+	},
+	{
+		name: 'Andrea',
+		photo: 'https://picture_Andrea.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'3',
+			'1',
+			'2',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Jose',
+		photo: 'http://picture_Jose.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Claudia',
+		photo: 'https://pictureClaudia.jpeg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Cloe',
+		photo: 'http://picture_Cloe.jpg',
+		scores: [
+			'1',
+			'2',
+			'5',
+			'2',
+			'5',
+			'4',
+			'5',
+			'3',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Samuel',
+		photo: 'http://picture_Samuel.jpg',
+		scores: [
+			'1',
+			'4',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Byron',
+		photo: 'http://picture_Byron.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Douglas',
+		photo: 'https://picture_Douglas.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Katty',
+		photo: 'http://picture_Katty.jpg',
+		scores: [
+			'5',
+			'5',
+			'4',
+			'5',
+			'3',
+			'5',
+			'2',
+			'5',
+			'3',
+			'5'
+		]
+	},
+	{
+		name: 'Mortimer',
+		photo: 'http://picture_Mortimer.jpg',
+		scores: [
+			'1',
+			'3',
+			'1',
+			'3',
+			'4',
+			'5',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	}
+];
+
+module.exports = friendsArray;
